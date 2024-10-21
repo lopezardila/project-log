ed5e99cacff18a3590e6d040d4b4baa574adbb1d
lopezardila <lopezardila01@gmail.com>
2024-10-20 21:21:02 -0300

    inital commit

--- src/components/layout/Landing.js
+++ src/components/layout/Landing.js
@@ -20,9 +20,9 @@ const {
 
 const basic = {
   apiKey: "AIzaSyDoyi01xdksbWgIWFWtaxj1R80DdZ6PWbw",
-  authDomain: "Gold-Dragon.firebaseapp.com",
-  projectId: "Gold-Dragon-784f0",
-  storageBucket: "Gold-Dragon.appspot.com",
+  authDomain: "project-log.firebaseapp.com",
+  projectId: "project-log-9da74",
+  storageBucket: "project-log.appspot.com",
   messagingSenderId: "879731928519",
   appId: "1:879731928519:web:875d6ddfbee3d1b0363de2",
   measurementId: "G-0KJ8T512FL",
